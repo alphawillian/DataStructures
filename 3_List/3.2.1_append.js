562f32485543ab58e5536ba1d559780ec090c117
william <willian126@126.com>
2018-01-05 01:59:09 +0800

    2018.1.05 数据结构列表学习
    2018.01.05 数据结构列表学习

--- 3_List/3.2.1_append.js
+++ 3_List/3.2.1_append.js
@@ -3,28 +3,117 @@ function list() {
     this.listSize = 0;
     this.pos = 0;
     this.dataStore = [];    //初始化一个空数组来保存列表元素
-    // this.clear = clear;
-    // this.find = find;
-    // this.toString = toString;
-    // this.insert = insert;
+    this.clear = clear;
+    this.find = find;
+    this.toString = toString;
+    this.insert = insert;
     this.append = append;
-    // this.remove = remove;
+    this.remove = remove;
     // this.front = front;
     // this.end = end;
     // this.prev = prev;
     // this.next = next;
     // this.hasNext;
     // this.hasPrev;
-    // this.length = length;
+    this.length = length;
     // this.currPos = currPos;
     // this.moveTo = moveTo;
     // this.getElement = getElement;
-    // this.contains = contains;
+    this.contains = contains;
 }
 
 function append(element) {
     this.dataStore[this.listSize++] = element;
 }
+function find(element) {
+    for(var i = 0; i < this.dataStore.length; i++){
+        if(this.dataStore[i] == element){
+            return i;
+        }
+    }
+    return -1;
+}
+function remove(element) {
+    var findAt = this.find(element);
+    if(findAt > -1){
+        this.dataStore.splice(findAt, 1);
+        --this.listSize;
+        return true;
+    }else{
+        return false;
+    }
+}
+function length() {
+    return this.listSize;
+}
+function toString() {
+    return this.dataStore;
+}
+function insert(element, after) {
+    var insertPos = this.find(after);
+    if(insertPos > -1){
+        this.dataStore.splice(insertPos+1, 0, element);
+        ++this.listSize;
+        return true;
+    }else{
+        return false;
+    }
+}
+function clear(){
+    delete this.dataStore;
+    // this.dataStore.length = 0;
+    this.dataStore = [];
+    this.listSize = this.pos = 0;
+}
+function contains(element) {
+    for (var i = 0; i < this.listSize; i++) {
+        if(this.dataStore[i] == element){
+            return true;
+        }
+    }
+    return false;
+}
+function front() {
+    this.pos = 0;
+}
+function end(){
+    this.pos = this.listSize - 1;
+}
+function prev() {
+    if(this.pos > 0){
+        this.pos--;
+    }else{
+        this.pos = 0;
+    }
+}
+function next() {
+    if(this.pos < this.listSize - 1){
+        this.pos++
+    }else{
+        this.pos = this.listSize - 1;
+    }
+}
+function currPos() {
+    return this.pos;
+}
+function moveTo(position) {
+    if(position < this.listSize -1){
+        this.pos = position;
+    }
+}
+
 let myList = new list();
 myList.append('1');
-console.log(myList);
+myList.append('2');
+myList.append('3');
+myList.append('4');
+console.log(myList.toString());
+myList.remove('3');
+console.log(myList.toString());
+myList.insert('5','2');
+console.log(myList.toString());
+console.log(myList.contains('2')?'2存在于列表中':'2不存在于列表中');
+console.log(myList.contains('1999')?'1999存在于列表中':'1999不存在于列表中');
+
+myList.clear();
+// console.log(myList);
