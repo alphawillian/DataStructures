3fc06bc0c0af61f98ed1ccce0265fc263aaf9841
william <willian126@126.com>
2018-01-08 00:18:07 +0800

    2018.1.8 es6

--- 3_List/es6.js
+++ 3_List/es6.js
@@ -9,3 +9,13 @@ let obj = {
 let {arr:[greet,{a}]} = obj;
 console.log('greet:', greet);
 console.log('a:', a);
+
+let arr = [1,2];
+
+function test([a,b]){
+    console.log('a:',a);
+    console.log('b:',b);
+}
+test(arr);
+let {length:len} = 'hello';
+console.log(len);
