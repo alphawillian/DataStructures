19b5b04a591e0617330d6d7179bbe414c373455e
william <willian126@126.com>
2018-01-10 00:47:18 +0800

    2018.1.10 es6

--- 3_List/es6.js
+++ 3_List/es6.js
@@ -11,11 +11,21 @@ console.log('greet:', greet);
 console.log('a:', a);
 
 let arr = [1,2];
+let obj1 = {
+    // a:1,
+    b:2
+}
 
 function test([a,b]){
     console.log('a:',a);
     console.log('b:',b);
 }
-test(arr);
+function testObj({b,a = 10}){
+    console.log('a:',a);
+    console.log('b:',b);
+}
+
+// test(arr);
+testObj(obj1);
 let {length:len} = 'hello';
 console.log(len);
